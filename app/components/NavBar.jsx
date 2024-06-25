f080cda15bbedcf81bf5bb6b7418d588ee7f0660
viisualworks <dshege4@gmail.com>
2024-06-25 15:25:06 +0300

    load tailwind css to my project

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,9 +2,12 @@ import Image from "next/image"
 import Logo from "../../public/images/logo.svg"
 
 export default function NavBar(){
-    return (
-        <nav className="">
-            <Image src={Logo} alt="" />
-        </nav>
-    )
+return (
+<nav className="">
+    <Image src={Logo} alt="" />
+    <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        Gotha!
+    </button>
+</nav>
+)
 }
