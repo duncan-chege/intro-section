45b4f0c2a19d5f40868d2e5d34eef7f562b8e96e
viisualworks <dshege4@gmail.com>
2024-06-27 19:57:43 +0300

    fix dual dropdown functions

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -6,25 +6,42 @@ import CalendarIcon from "../../public/images/icon-calendar.svg"
 import PlanningIcon from "../../public/images/icon-planning.svg"
 import RemindersIcon from "../../public/images/icon-reminders.svg"
 import TodoIcon from "../../public/images/icon-todo.svg"
+import ArrowDown from "../../public/images/icon-arrow-down.svg"
+
 
 import { useState, useEffect, useRef } from "react";
 
 export default function NavBar(){
-    const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null);
+    const [dropdowns, setDropdowns] = useState({
+      features: false,
+      company: false,
+    });
   
-    const toggleDropdown = () => {
-      setIsOpen(!isOpen);
+    const dropdownRefs = {
+      features: useRef(null),
+      company: useRef(null),
     }
+
+    const toggleDropdown = (name) => {
+      setDropdowns((prevDropdowns) => ({
+        ...prevDropdowns,
+        [name]: !prevDropdowns[name],
+      }));
+    }
+
   
     // this is triggered whenever a mouse clicks occurs anywhere on the document
     const handleClickOutside = (event) => {
-
-      // dropdownRef.current.contains(event.target) checks if the clicked element is inside the dropdown.
-      if( !dropdownRef.current.contains(event.target)){
-        setIsOpen(false);
+      for (const key in dropdownRefs) {
+        // dropdownRef.current.contains(event.target) checks if the clicked element is inside the dropdown.
+        if( !dropdownRefs[key].current.contains(event.target)){
+          setDropdowns((prevDropdowns) => ({
+            ...prevDropdowns,
+            [key]: false,
+          }));
+        }
+        };
       }
-    };
   
     useEffect(() => {
       document.addEventListener('mousedown', handleClickOutside);
@@ -36,58 +53,86 @@ export default function NavBar(){
   
   return (
   <nav className="bg-transparent m-8 flex flex-row justify-between">
-    <Image className="flex-none shrink-0" src={Logo} alt="" />
-    <div className="flex flex-row w-2/5 justify-evenly">
-      <div className="relative inline-block text-left" ref={dropdownRef}>
-        <div>
+   <Image className="flex-none shrink-0" src={Logo} alt="" /> {/* Assuming Logo is imported and defined */}      
+      <div className="flex flex-row w-2/5 justify-evenly">
+        
+        {/* First Dropdown */}
+        <div className="relative inline-block text-left" ref={dropdownRefs.features}>
           <button 
             type="button"
-            className="inline-flex w-full justify-center gap-x-1.5 text-base text-gray-900"
+            className="inline-flex w-full justify-center gap-x-1.5 text-base text-medium-gray hover:text-almost-black"
             id="menu-button"
-            aria-expanded={isOpen}
+            aria-expanded={dropdowns.features}
             aria-haspopup="true"
-            onClick={toggleDropdown} > Features
-            
-            <svg 
-              className="-ml-1 h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true" >
-              <path fillRule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clipRule="evenodd" />
-            </svg>
+            onClick={() => toggleDropdown('features')}
+          >
+            Features
+            <Image src={ArrowDown} className="mt-2"/>
           </button>
-        </div>
 
-  {isOpen && (
-        <div
-          className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-2 ring-medium-gray ring-opacity-5 focus:outline-none"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button" >
-
-        <div className="py-1" role="none">
-            <a href="#" className="flex px-4 py-2 text-sm text-medium-gray w-max" role="menuitem" tabIndex="-1"
-              id="menu-item-0"><span className="pr-2"><Image src={TodoIcon} /></span>To do list</a>
-            <a href="#" className="flex px-4 py-2 text-sm text-medium-gray w-max" role="menuitem" tabIndex="-1"
-              id="menu-item-1"><span className="pr-2"><Image src={CalendarIcon} /></span>Calendar</a>
-            <a href="#" className="flex px-4 py-2 text-sm text-medium-gray w-max" role="menuitem" tabIndex="-1"
-              id="menu-item-2"><span className="pr-2"><Image src={RemindersIcon} /></span>Reminders</a>
-            <a href="#" className="flex px-4 py-2 text-sm text-medium-gray w-max" role="menuitem" tabIndex="-1"
-              id="menu-item-2"><span className="pr-2"><Image src={PlanningIcon} /></span>Planning</a>
+        {dropdowns.features && (
+          <div
+            className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-2 ring-medium-gray ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button-features"
+          >
+            <div className="py-1" role="none">
+              <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                id="menu-item-features-0"><span className="pr-2"><Image src={TodoIcon} /></span>To do list</a>
+              <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                id="menu-item-features-1"><span className="pr-2"><Image src={CalendarIcon} /></span>Calendar</a>
+              <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                id="menu-item-features-2"><span className="pr-2"><Image src={RemindersIcon} /></span>Reminders</a>
+              <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                id="menu-item-features-3"><span className="pr-2"><Image src={PlanningIcon} /></span>Planning</a>
+            </div>
           </div>
+        )}
         </div>
-      )}
-      </div>
 
-      <p className="font-sans"> Company </p>
-      <p> Careers </p>
-      <p> About </p>
-    </div>
+
+        {/* Second Dropdown */}
+        <div className="relative inline-block text-left" ref={dropdownRefs.company}>
+          <div>
+            <button 
+              type="button"
+              className="inline-flex w-full justify-center gap-x-1.5 text-base text-medium-gray hover:text-almost-black"
+              id="menu-button-company"
+              aria-expanded={dropdowns.company}
+              aria-haspopup="true"
+              onClick={() => toggleDropdown('company')}
+            >
+              Company
+              <Image src={ArrowDown} className="mt-2"/>
+            </button>
+          </div>
+
+          {dropdowns.company && (
+            <div
+              className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-2 ring-medium-gray ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button-company"
+            >
+              <div className="py-1" role="none">
+                <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                  id="menu-item-company-0">History</a>
+                <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                  id="menu-item-company-1">Our Team</a>
+                <a href="#" className="flex px-4 py-2 text-sm text-medium-gray hover:text-almost-black w-max" role="menuitem" tabIndex="-1"
+                  id="menu-item-company-2">Blog</a>
+              </div>
+            </div>
+          )}
+        </div>
+        <a href="#" className="text-medium-gray hover:text-almost-black"> Careers </a>
+        <a href="#" className="text-medium-gray hover:text-almost-black"> About </a>
+      </div>
+  
     <div className="flex flex-row w-1/5 justify-evenly ml-auto">
-      <p>Login</p>
-      <p>Register</p>
+      <button className="text-medium-gray hover:text-almost-black">Login</button>
+      <button className="text-medium-gray hover:text-almost-black border-2 border-medium-gray hover:border-almost-black px-2.5 py-1.5 rounded-lg">Register</button>
     </div>
   </nav>
 
